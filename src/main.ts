49bc587f1c5b4550acb27220a9a252fe90dc4749
Minhthuctp <thuctanphu12@gmail.com>
2024-04-28 15:09:01 +0700

    [fix]: Fix to show UI of API doc

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+  });
   await app.listen(3001);
 }
 
